b92cbdea09928d1e5125a3808f377954c495d87d
vijaytripathi <vijayt@microsoft.com>
2015-05-12 23:33:13 +0000

    adding script to setup virtual machines by adding power type and changing hostname to its vmname

---
+++ scripts/auto-setup-vm.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import sys, os, libvirt
+from xml.dom.minidom import parseString
+os.environ['DJANGO_SETTINGS_MODULE'] = 'maas.settings'
+sys.path.append("/usr/share/maas")
+from maasserver.models import Node, Tag
+
+kvm_host = 'qemu+ssh://ubuntu@10.16.0.28/system'
+
+conn = libvirt.open(kvm_host)
+dom_dict = {}
+domains = conn.listDefinedDomains()
+
+#print domains
+for dom_name in domains:
+    node = conn.lookupByName(dom_name)
+    dom_xml = parseString(node.XMLDesc(0))
+    dom_mac1 = dom_xml.getElementsByTagName('interface')[0].getElementsByTagName('mac')[0].getAttribute('address')
+    dom_dict[dom_mac1] = dom_name
+    #print dom_dict
+
+maas_nodes = Node.objects.all()
+#print maas_nodes
+for node in maas_nodes:
+    try:
+        system_id = node.system_id
+        mac = node.get_primary_mac()
+        if str(mac) in dom_dict:
+#        while ( str(mac) in dom_dict ):
+          print node
+          dom_name = dom_dict[str(mac)]
+          node.hostname = dom_name
+          node.power_type = 'virsh'
+          node.power_parameters = { 'power_address':kvm_host, 'power_id':dom_name }
+          node.save()
+    except: 
+        pass
